ed515d9e29287300418b9291cefc7dfeb6cc9561
BowieHHY <2295285120@qq.com>
2021-05-23 16:00:26 +0800

    add mongoDB connect

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI:'mongodb+srv://hhy:hhy123123@cluster0.rjhk0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+const db=require('./config/keys').MongoURI
+mongoose
+  .connect(db,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { console.log('mongodb connected') })
+  .catch((err)=>{console.log(err)})
 
 const PORT = process.env.PORT || 3000
 
