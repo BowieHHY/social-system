78a63ef746ddceadcbaedd8ad44435bd0155bf36
BowieHHY <2295285120@qq.com>
2021-05-23 22:00:11 +0800

    update : 搭建登录接口

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,4 +51,29 @@ router.post("/register", (req, res) => {
     })
 })
 
+// $route GET api/users/login
+// @desc token
+// @access public
+router.post("/login", (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  // 查询数据库
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({email:'该用户不存在！'})
+      }
+
+      //密码匹配
+      bcrypt.compare(password, user.password).then((isMatch) => {
+        if (isMatch) {
+          res.json({msg:'success'})
+        } else {
+          return res.status(400).json({password:'密码错误'})
+        }
+      })
+    })
+})
+
 module.exports = router
